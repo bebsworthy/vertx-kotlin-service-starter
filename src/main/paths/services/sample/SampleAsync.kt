089a3595888055ee5481fa9b2cbde2c7a0fdf53b
boyd <bebsworthy@gmail.com>
2018-05-24 18:15:06 +0400

    * Example of async calls on a service using coroutines

---
+++ src/main/paths/services/sample/SampleAsync.kt
@@ -0,0 +1,131 @@
+package paths.services.sample
+
+
+import io.vertx.codegen.annotations.ProxyClose
+import io.vertx.codegen.annotations.ProxyGen
+import io.vertx.codegen.annotations.VertxGen
+import io.vertx.core.AsyncResult
+import io.vertx.core.Future
+import io.vertx.core.Handler
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.coroutines.awaitResult
+import io.vertx.serviceproxy.ServiceBinder
+import io.vertx.serviceproxy.ServiceProxyBuilder
+import kotlinx.coroutines.experimental.async
+import paths.services.AbstractBusServiceVerticle
+import paths.services.auth.AuthService
+import java.util.logging.Logger
+
+
+/**
+ * Simple interface for the service
+ */
+@ProxyGen
+@VertxGen
+interface SampleAsyncService {
+
+    fun levelOne(data: String, resultHandler: Handler<AsyncResult<String>>)
+
+    fun levelTwo(data: String, resultHandler: Handler<AsyncResult<JsonObject>>)
+
+    @ProxyClose
+    fun close()
+}
+
+/**
+ * Implementation of the service
+ */
+data class SampleAsyncServiceImpl(val vertx: Vertx) : SampleAsyncService {
+    private val log: Logger = Logger.getLogger(SampleAsyncServiceImpl::class.qualifiedName)
+
+    /**
+     * LevelOne function
+     */
+    override fun levelOne(data: String, resultHandler: Handler<AsyncResult<String>>) {
+        println("Begin levelOne $data")
+
+        levelTwo("$data.one", Handler { f ->
+
+            if (f.succeeded()) {
+                val r = f.result()
+                println("LevelOne: result from levelTwo: $r")
+                // were done so we notify with a success
+                resultHandler.handle(Future.succeededFuture("ok"))
+            } else {
+                println("LevelOne: levelTwo Failed")
+                // It failed so we return a failure and propagate the reason
+                resultHandler.handle(Future.failedFuture(f.cause()))
+            }
+        })
+
+        println("End levelOne $data function")
+    }
+
+    /**
+     * LevelOne function
+     * Demonstrate how to call an asynchronous method using the coroutine style
+     */
+    override fun levelTwo(data: String, resultHandler: Handler<AsyncResult<JsonObject>>) {
+        // Here we will call the auth service three time sequentially
+
+        println("Begin levelTwo")
+
+        val builder = ServiceProxyBuilder(vertx).setAddress("auth-service")
+        val service = builder.build(AuthService::class.java)
+
+        val res = JsonObject()
+
+        async {
+
+            var i = 3
+            while (i > 0) {
+                println("levelTwo loop $i")
+                i--
+                try {
+                    val reply = awaitResult<String> { f ->
+                        service.authenticate("dora", "dora", f)
+                    }
+                    res.put("token$i", reply)
+                    println("result from authenticate $reply")
+                } catch (e: Exception) {
+                    println("authenticate failed: $e")
+                }
+                println("levelTwo end loop $i")
+            }
+
+            // Now we can complete the function here because it's in a coroutine block (async)
+            resultHandler.handle(Future.succeededFuture(res))
+        }
+
+        println("End levelTwo function")
+    }
+
+    @Override
+    override fun close() {
+        println("the close() override")
+    }
+}
+
+/**
+ * Simple verticle to connect our service to the bus
+ */
+class SampleAsyncServiceVerticle : AbstractBusServiceVerticle() {
+    companion object {
+        const val SERVICE_ADDRESS = "service-sample-async"
+    }
+
+    private val log = Logger.getLogger(this::class.qualifiedName)
+
+
+    override fun start(startFuture: Future<Void>) {
+        log.info("Starting SampleAsyncServiceVerticle...")
+
+        val service = SampleAsyncServiceImpl(vertx)
+
+        consumer = ServiceBinder(vertx).setAddress(SERVICE_ADDRESS)
+                .register(SampleAsyncService::class.java, service)
+
+        startFuture.complete()
+    }
+}
---
+++ src/test/paths/services/sample/SampleAsyncServiceTest.kt
@@ -0,0 +1,102 @@
+package paths.services.sample
+
+import io.vertx.core.CompositeFuture
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Future
+import io.vertx.core.Vertx
+import io.vertx.junit5.VertxExtension
+import io.vertx.junit5.VertxTestContext
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
+import io.vertx.serviceproxy.ServiceProxyBuilder
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.extension.ExtendWith
+import paths.services.auth.AuthVerticle
+
+
+/**
+ * A simple JUnit5 test class for a vertx project
+ */
+@ExtendWith(VertxExtension::class)
+class SampleAsyncServiceTest {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun initAll() {
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDownAll() {
+        }
+    }
+
+    private fun getService(vertx: Vertx): SampleAsyncService {
+        val builder = ServiceProxyBuilder(vertx).setAddress(SampleAsyncServiceVerticle.SERVICE_ADDRESS)
+        return builder.build(SampleAsyncService::class.java)
+    }
+
+
+    /** deploy a bunch of verticles */
+    private fun deploy(vertx: Vertx, verticleName: String, options: DeploymentOptions): Future<Any> {
+        val returnval = Future.future<Any>()
+
+        vertx.deployVerticle(verticleName, options, { res ->
+            if (res.succeeded()) {
+                println("Deployed ok: $verticleName")
+                returnval.complete()
+            } else {
+                println("Deploy failed! $verticleName")
+                returnval.fail(res.cause())
+            }
+
+        })
+
+        return returnval
+    }
+
+    /**
+     * This is called before each test and deploys a fresh AuthenticationVerticle
+     */
+    @BeforeEach
+    fun `Deploy Verticle`(vertx: Vertx, testContext: VertxTestContext) {
+
+        val options = DeploymentOptions()
+                .setConfig(
+                        json {
+                            obj(
+                                    "auth.keystore" to "security-dev/keystore.pkcs12",
+                                    "auth.keystore.password" to "secret"
+                            )
+                        }
+                )
+
+        CompositeFuture.all(
+                deploy(vertx, AuthVerticle::class.qualifiedName ?: "", options),
+                deploy(vertx, SampleAsyncServiceVerticle::class.qualifiedName ?: "", options)
+        ).setHandler(testContext.succeeding {
+            testContext.completeNow()
+        })
+    }
+
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    /**
+     * test level one
+     */
+    @Test
+    fun `Test levelOne`(vertx: Vertx, testContext: VertxTestContext) {
+        var service = getService(vertx)
+        var data = "dora"
+        service.levelOne(data,
+                testContext.succeeding {
+                    assertEquals(it, "ok")
+                    testContext.completeNow()
+                })
+    }
+}
